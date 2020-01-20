af67fc20b4f0cf1f11c5f5014392e53dfa191e51
Ruben Zagon <ruben.zagon@gmail.com>
2020-01-20 10:13:21 +0000

    Initial build subtree commit

---
+++ precache-manifest.1d873563175da4e2b69117febd4f1db4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9963b2947a4f73679f9035212864484c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3205dcc16c5636b76034",
+    "url": "/static/css/main.97c6d0f2.chunk.css"
+  },
+  {
+    "revision": "20a0c7e9870d73b60eb9",
+    "url": "/static/js/2.87c7fbde.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.87c7fbde.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3205dcc16c5636b76034",
+    "url": "/static/js/main.278a421a.chunk.js"
+  },
+  {
+    "revision": "7ebe5cdd7d278cf54f61",
+    "url": "/static/js/runtime-main.44184d1d.js"
+  }
+]);
